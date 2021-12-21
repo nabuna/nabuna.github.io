70b3776b11190d19fa0619cd8cb3423b20ac49e7
Adrian <adrianmontero@tutanota.com>
2021-12-21 05:41:47 +0100

    update

---
+++ ikanaide/main.js
@@ -0,0 +1,10 @@
+const header_profile = document.querySelector("#profile") ;
+const header_profile_dropdown = document.getElementById("profile-dropdown") ;
+
+function toggle() {
+    if (header_profile_dropdown.style.display == "none") {
+
+        header_profile_dropdown.style.display = "block" ;
+
+        }
+    }
